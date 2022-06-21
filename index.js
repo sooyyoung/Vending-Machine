d591adcd1e6fa74061ad74bee82d41303384e053
sooyyoung <sooyyyoung@gmail.com>
2022-06-21 13:17:38 +0900

    Docs. 입금액 반영 수정

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const depositInput = document.querySelector('.deposit-input');
 const depositBtn = document.querySelector('.deposit-btn');
 const pocketMoney = document.querySelector('.money-txt');
 
+let result = 0
+
 // 1. 소지금 입금하기
 // 1-1. 금액이 빈 상태로 입금 버튼 클릭 시 입금 안내 창 출력
 depositBtn.addEventListener('click', () => {
     if(depositInput.value == ''){
         alert('입금액을 입력해주세요!')
     } else {
-        // 1-2. 입금액 입력 시 소지금에 반영
-        pocketMoney.textContent = depositInput.value
-        depositInput.value = ''
+    // 1-2. 입금액 입력 시 소지금에 반영
+    let money = parseInt(depositInput.value)
+    result += money
+    pocketMoney.innerHTML = result.toLocaleString();
+    depositInput.value = ''
     }
 })
